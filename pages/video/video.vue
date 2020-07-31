f72af319aa6ac6a00b5722317f3733f7fc4602c3
xiaonian <xiaonian0430@163.com>
2020-07-31 08:36:57 +0800

    h5box

--- pages/video/video.vue
+++ pages/video/video.vue
@@ -1,93 +1,134 @@
 <template>
 	<view>
 		<page-head :title="title"></page-head>
-		<view class="uni-padding-wrap uni-common-mt" v-if="showVideo">
-			<view>
-				<video id="myVideo" src="https://xnall.oss-cn-hangzhou.aliyuncs.com/ets-course-app/dev/20200721/ios_10_27_13_057_movie.mp4"
-				 @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls poster="https://img-cdn-qiniu.dcloud.net.cn/uniapp/doc/poster.png"></video>
-			</view>
-			<!-- #ifndef MP-ALIPAY || MP-TOUTIAO -->
-			<view class="uni-list uni-common-mt">
+		<view class="uni-common-mt">
+			<view class="uni-list">
 				<view class="uni-list-cell">
-					<view>
-						<view class="uni-label">弹幕内容</view>
+					<view class="uni-list-cell-left">
+						<view class="uni-label">视频来源</view>
 					</view>
-					<view class="uni-list-cell-db">
-						<input v-model="danmuValue" class="uni-input" type="text" placeholder="在此处输入弹幕内容" />
+					<view class="uni-list-cell-right">
+						<picker :range="sourceType" @change="sourceTypeChange" :value="sourceTypeIndex">
+							<view class="uni-input">{{sourceType[sourceTypeIndex]}}</view>
+						</picker>
 					</view>
 				</view>
 			</view>
-			<view class="uni-btn-v">
-				<button @click="sendDanmu" class="page-body-button">发送弹幕</button>
-			</view>
-			<!-- #endif -->
 		</view>
-	</view>
+		<!-- #ifdef APP-PLUS || MP-WEIXIN -->
+		<view class="uni-title uni-common-mt uni-common-pl">摄像头位置</view>
+		<view class="uni-hello-text camera-tips">注意：部分 Android 手机下由于系统 ROM 不支持无法生效，打开拍摄界面后可操作切换</view>
+		<view class="uni-list">
+			<radio-group @change="radioChange">
+				<label class="uni-list-cell uni-list-cell-pd" v-for="(item, index) in cameraList" :key="item.value">
+					<radio :value="item.value" :checked="index === cameraIndex">{{item.name}}</radio>
+				</label>
+			</radio-group>
+		</view>
+		<!-- #endif -->
+		<template v-if="!src">
+			<view class="uni-hello-addfile" @tap="chooseVideo">+ 添加视频</view>
+		</template>
+		<template v-else>
+			<video :src="src" class="video"></video>
+		</template>
 	</view>
 </template>
 <script>
+	var sourceType = [
+		['camera'],
+		['album'],
+		['camera', 'album']
+	]
 	export default {
 		data() {
 			return {
-				title: '原始视频',
+				title: 'chooseVideo',
+				sourceTypeIndex: 2,
+				sourceType: ['拍摄', '相册', '拍摄或相册'],
 				src: '',
-				danmuList: [{
-						text: '第 1s 出现的弹幕',
-						color: '#ff0000',
-						time: 1
+				cameraList: [{
+						value: 'back',
+						name: '后置摄像头',
+						checked: 'true'
 					},
 					{
-						text: '第 3s 出现的弹幕',
-						color: '#ff00ff',
-						time: 3
-					}
+						value: 'front',
+						name: '前置摄像头'
+					},
 				],
-				danmuValue: '',
-				showVideo: false
+				cameraIndex: 0
 			}
 		},
-		onReady: function(res) {
-			// #ifndef MP-ALIPAY || MP-TOUTIAO
-			this.videoContext = uni.createVideoContext('myVideo')
-			// #endif
-      // #ifdef APP-PLUS || MP-BAIDU
-      setTimeout(()=>{
-      	this.showVideo = true
-      },350)
-      // #endif
-      // #ifndef APP-PLUS || MP-BAIDU
-      this.showVideo = true
-      // #endif
+		onUnload() {
+			this.src = '',
+				this.sourceTypeIndex = 2,
+				this.sourceType = ['拍摄', '相册', '拍摄或相册'];
 		},
 		methods: {
-			sendDanmu: function() {
-				this.videoContext.sendDanmu({
-					text: this.danmuValue,
-					color: this.getRandomColor()
-				});
-				this.danmuValue = '';
-			},
-			videoErrorCallback: function(e) {
-				uni.showModal({
-					content: e.target.errMsg,
-					showCancel: false
-				})
-			},
-			getRandomColor: function() {
-				const rgb = []
-				for (let i = 0; i < 3; ++i) {
-					let color = Math.floor(Math.random() * 256).toString(16)
-					color = color.length == 1 ? '0' + color : color
-					rgb.push(color)
+			radioChange(evt) {
+				for (let i = 0; i < this.cameraList.length; i++) {
+					if (this.cameraList[i].value === evt.detail.value) {
+						this.cameraIndex = i;
+						break;
+					}
 				}
-				return '#' + rgb.join('')
+			},
+			sourceTypeChange: function(e) {
+				this.sourceTypeIndex = parseInt(e.detail.value)
+			},
+			chooseVideo: function() {
+				uni.chooseVideo({
+					camera: this.cameraList[this.cameraIndex].value,
+					sourceType: sourceType[this.sourceTypeIndex],
+					success: (res) => {
+						this.src = res.tempFilePath
+					},
+					fail: (err) => {
+						// #ifdef MP
+						uni.getSetting({
+							success: (res) => {
+								let authStatus = false;
+								switch (this.sourceTypeIndex) {
+									case 0:
+										authStatus = res.authSetting['scope.camera'];
+										break;
+									case 1:
+										authStatus = res.authSetting['scope.album'];
+										break;
+									case 2:
+										authStatus = res.authSetting['scope.album'] && res.authSetting['scope.camera'];
+										break;
+									default:
+										break;
+								}
+								if (!authStatus) {
+									uni.showModal({
+										title: '授权失败',
+										content: 'Hello uni-app需要从您的相机或相册获取视频，请在设置界面打开相关权限',
+										success: (res) => {
+											if (res.confirm) {
+												uni.openSetting()
+											}
+										}
+									})
+								}
+							}
+						})
+						// #endif
+					}
+				})
 			}
 		}
 	}
 </script>
 
 <style>
-	video {
-		width: 750rpx;
+	.video {
+		width: 100%;
+	}
+
+	.camera-tips {
+		padding: 10rpx 30rpx;
 	}
 </style>
